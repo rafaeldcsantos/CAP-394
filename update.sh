00894e7565f004267c39cf002d80c46715a4a5de
Rafael Santos <rafaeldcsantos@gmail.com>
2024-06-01 11:03:38 -0300

    Update changes

--- update.sh
+++ update.sh
@@ -2,14 +2,5 @@
 
 # Rerun quarto
 sass Resources/CSS/custom.scss Resources/CSS/custom.css
-quarto render --to html --verbose
+quarto render --to html
 
-
-# Add all changes
-git add .
-
-# Commit changes with a default message
-git commit -m "Update changes"
-
-# Push changes to the main branch
-git push origin main
