bd428348055862cf78b31a5028764569485556fc
Rafael Santos <rafaeldcsantos@gmail.com>
2024-07-04 23:27:00 -0300

    Update changes

--- .vdoc.py
+++ .vdoc.py
@@ -9,107 +9,158 @@
 #
 #
 #
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
 #| classes: custompython
 import pandas as pd
-import re
-import warnings
-import networkx as nx
-#
-#
-#
-#| classes: custompython
-dir = "Resources/Data/CAPES-Projetos/"
-with warnings.catch_warnings():
-    warnings.filterwarnings("ignore", category=UserWarning, 
-                            module=re.escape('openpyxl.styles.stylesheet'))
-    dfProjetos = pd.read_excel(dir+"br-capes-colsucup-projeto-2020-2021-11-10.xlsx")
-    dfMembros = pd.read_excel(dir+"br-capes-colsucup-membro-2020-2021-11-10.xlsx")
-#
-#
-#
-#
-#| classes: custompython
-dfProjetos = dfProjetos[
-    (dfProjetos['NM_TIPO_SITUACAO_ATUAL']=='EM ANDAMENTO') &
-    (dfProjetos['SG_ENTIDADE_ENSINO'] == 'INPE')]
-     
-dfProjetos = dfProjetos[['AN_BASE','NM_PROGRAMA_IES','AN_INICIO_PROGRAMA',
-                         'SG_ENTIDADE_ENSINO',
-                         'NM_AREA_AVALIACAO','NM_MODALIDADE_PROGRAMA',
-                         'NM_AREA_CONCENTRACAO','ID_PROJETO','NM_PROJETO',
-                         'NM_NATUREZA_PROJETO','DH_INICIO','DT_SITUACAO_ATUAL',
-                         'IN_BOLSA','IN_OUTRO_AUXILIO']]
-dfMembros = dfMembros[['ID_PROJETO','IN_RESPONSAVEL_PROJETO','ID_PESSOA',
-                       'DS_TIPO_MEMBRO','NM_PAIS_NACIONALIDADE_MEMBRO']]
-#
-#
-#
-#| classes: custompython
-dfUnião = pd.merge(dfProjetos, dfMembros, on='ID_PROJETO', how='inner')
-#
-#
-#
-#| classes: custompython
-print(dfProjetos.shape)
-print(dfMembros.shape)
-print(dfUnião.shape)
-#
-#
-#
-#
-#| classes: custompython
-dfUnião.to_csv(dir+'INPE2020.csv', index=False)
+import plotly.express as px
 #
 #
 #
 #
 #
 #| classes: custompython
-dfRede = dfUnião[['ID_PROJETO', 'ID_PESSOA', 'NM_PROJETO', 'DS_TIPO_MEMBRO']]
-dfRede.to_csv(dir+'INPE2020-Rede.csv', index=False)
+file = 'Resources/Data/Emendas/Emendas.csv'
+# Read the CSV file using the specified separator and encoding.
+df = pd.read_csv(file, sep=';', encoding='ISO-8859-1')
+# Convert the Valor field to numeric, taking care of the decimals'
+# separator:
+df['Valor'] = df['Valor Empenhado'].str.replace('.', '', regex=False).\
+                                    str.replace(',', '.').astype(float)
+# We don't really need some of the fields (redundant ones):
+df = df.drop(['Código da Emenda', 'Nome do Autor da Emenda', 'Número da emenda',
+              'Código Função','Código Subfunção'], axis=1)
+# We don't really need some of the payment fields (only 'Valor'):
+df = df.drop(['Valor Empenhado', 'Valor Liquidado', 'Valor Pago', 
+              'Valor Restos A Pagar Inscritos','Valor Restos A Pagar Cancelados',
+              'Valor Restos A Pagar Pagos'], axis=1)                                    
+# Display part of the data.
+df
+#
+#
+#
+#
+#
+#| classes: custompython
+countLoG = df["Localidade do gasto"].value_counts()
+# Print the result
+print(countLoG)
+#
+#
+#
+#
+#
+#| classes: custompython
+countNF = df["Nome Função"].value_counts()
+# Print the result
+print(countNF)
+#
+#
+#
+#
+#
+#| classes: custompython
+countAdE = df["Código do Autor da Emenda"].value_counts()
+# Print the result
+print(countAdE)
+#
+#
+#
+#
+#
+#| classes: custompython
+dfId = df[df["Código do Autor da Emenda"] != "S/I"]
+#
+#
+#
+#
+#
+#| classes: custompython
+dfNacional = dfId[dfId["Localidade do gasto"] == "Nacional"]
+dfStates = dfId[dfId["Localidade do gasto"].str.contains(r"\(\w{2}\)", regex=True)]
+#
+#
+#
+#
+#
+#| classes: custompython
+dfDefesaNacional = dfNacional[dfNacional["Nome Função"] == "Defesa nacional"]
+dfValueByYear = dfDefesaNacional.groupby("Ano da Emenda")["Valor"].sum().reset_index()
+# Create a histogram
+fig = px.bar(dfValueByYear, x='Ano da Emenda', y='Valor', 
+             title='Total Values per Year in National Defense',
+             labels={'Ano da Emenda': 'Year', 'Valor': 'Total Value'})
+# Show the plot
+fig.show()
+#
+#
+#
+#
+#
+#
+#| classes: custompython
+subfuncCounts = dfDefesaNacional["Nome Subfunção"].value_counts()
+print("\nOccurrences of each unique 'Nome Subfunção':")
+print(subfuncCounts)
+#
 #
 #
 #
 #
 #| classes: custompython
 
-# Create the graph
-G = nx.Graph()
-
-# Add nodes with attributes
-for _, row in df.iterrows():
-    G.add_node(row['ID_PESSOA'], label=row['ID_PESSOA'], tipo_membro=row['DS_TIPO_MEMBRO'])
-
-# Add edges based on project collaboration
-projects = df.groupby('ID_PROJETO')['ID_PESSOA'].apply(list)
-for project, participants in projects.items():
-    for i in range(len(participants)):
-        for j in range(i + 1, len(participants)):
-            G.add_edge(participants[i], participants[j], project=project)
-
-            
-
-nx.write_gexf(G,dir+"INPE2020-Rede.gexf")
-
-
+# Create a pivot table to see spending per year on different "Nome Subfunção"
+pivot_table = dfDefesaNacional.pivot_table(
+    values='Valor',
+    index='Ano da Emenda',
+    columns='Nome Subfunção',
+    aggfunc='sum',
+    fill_value=0
+)
+pivot_table
 #
 #
 #
 #
 #
-#| classes: custompython
 #
 #
-#
-#
-#
-#| classes: custompython
-#
-#
-#
-#
-#
-#| classes: custompython
 #
 #
 #
