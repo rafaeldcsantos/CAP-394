9296997a437f15b9a8c6ef7303d28a80b8a8f94f
Rafael Santos <rafaeldcsantos@gmail.com>
2025-06-21 21:09:43 -0300

    Update changes

--- upload.sh
+++ upload.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+large_files=$(find . -path ./.git -prune -o -type f -size +90M -print)
+
+if [[ -n "$large_files" ]]; then
+  echo "❌ The following files are larger than 90MB and will block the push:"
+  echo "$large_files"
+  echo "Please remove them or use Git LFS."
+  exit 1
+fi
+
 
 # Add all changes
 git add .
